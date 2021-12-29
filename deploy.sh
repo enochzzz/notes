5bfab67cfe834750f685a9cbf45db3e2497051f3
Enoch <qhxngzx@gmail.com>
2021-12-29 23:10:17 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,6 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:Enochzzz/notes.git master
+git push -f git@github.com:Enochzzz/notes.git master:gh-pages
 
 cd -
